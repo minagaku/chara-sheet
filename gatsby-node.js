2f66f7e08b68e6b614b69fb59ac82c9b5f0e548c
GenKuzumochi <GenKuzumochi@gmail.com>
2020-04-28 00:19:01 +0900

    Revert "remove getsby-node.js"
    This reverts commit 218486d37a98f6369de6a40951d993468256725c.

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,11 +6,11 @@
  */
 
 // You can delete this file if you're not using it
-// exports.onCreatePage = ({ page, actions }) => {
-//   const { createPage } = actions;
-//   if (page.path === `/`) {
-//     page.matchPath = `/*`;
-//     createPage(page);
-//   }
-// };
+exports.onCreatePage = ({ page, actions }) => {
+  const { createPage } = actions;
+  if (page.path === `/`) {
+    page.matchPath = `/*`;
+    createPage(page);
+  }
+};
 
